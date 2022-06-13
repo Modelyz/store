bb06f08dd447de865e7100502106c3bc13352503
Christophe Combelles <ccomb@free.fr>
2022-06-13 11:47:34 +0200

    init project

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+BUILD="cabal build"
+DEVEL='--ghc-options="-Wall"'
+OPTIMIZE=" -O2 --enable-split-objs --enable-executable-stripping --enable-library-stripping --enable-executable-dynamic"
+DIR="dist-newstyle/build/x86_64-linux/"
+
+pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p build
+
+if [ "$1" == "-o" ]; then
+    $BUILD $OPTIMIZE
+    find $DIR -type f -name store -exec cp {} build/ \;
+else
+    $BUILD $DEVEL
+    find $DIR -type f -name store -exec cp {} build/ \;
+fi
+popd
