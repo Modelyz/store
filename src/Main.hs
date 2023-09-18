c360779c454594d026fe9b2245154e0cea2f9435
Christophe Combelles <ccomb@free.fr>
2023-09-18 08:18:39 +0200

    myself to avoid copy-paste errors

--- src/Main.hs
+++ src/Main.hs
@@ -23,14 +23,13 @@ import Data.Set as Set (Set, empty, insert)
 import Message (Message (..), Payload (..), addVisited, appendMessage, creator, getFlow, metadata, payload, readMessages)
 import MessageFlow (MessageFlow (..))
 import MessageId (MessageId, messageId)
-import Metadata (Metadata (Metadata), Origin (..), flow, from, uuid, when)
+import Metadata (Metadata (Metadata), flow, from, uuid, when)
 import Network.WebSockets qualified as WS
 import Options.Applicative
+import Service (Service (..))
 
 -- port, file
 data Options = Options FilePath Host Port
-
-type Client = Origin
 
 type Host = String
 
@@ -44,6 +43,9 @@ data State = State
 
 type StateMV = MVar State
 
+myself :: Service
+myself = Store
+
 emptyState :: State
 emptyState = State{pending = Map.empty, Main.uuids = Set.empty}
 
@@ -54,22 +56,22 @@ options =
         <*> strOption (short 'h' <> long "host" <> value "localhost" <> help "Bind socket to this host. [default: localhost]")
         <*> option auto (short 'p' <> long "port" <> metavar "PORT" <> value 8081 <> help "Bind socket to this port.  [default: 8081]")
 
-syncBackMessage :: WS.Connection -> Client -> Message -> IO ()
+syncBackMessage :: WS.Connection -> Service -> Message -> IO ()
 syncBackMessage conn client msg = do
     -- sync back the messages to an empty service
     case client of
         Ident -> case payload msg of
             InitiatedConnection _ -> return ()
             -- send to ident :
-            AddedIdentifierType _ -> WS.sendTextData conn $ JSON.encode $ addVisited Store msg
-            RemovedIdentifierType _ -> WS.sendTextData conn $ JSON.encode $ addVisited Store msg
-            ChangedIdentifierType _ _ -> WS.sendTextData conn $ JSON.encode $ addVisited Store msg
-            AddedIdentifier _ -> WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+            AddedIdentifierType _ -> WS.sendTextData conn $ JSON.encode $ addVisited myself msg
+            RemovedIdentifierType _ -> WS.sendTextData conn $ JSON.encode $ addVisited myself msg
+            ChangedIdentifierType _ _ -> WS.sendTextData conn $ JSON.encode $ addVisited myself msg
+            AddedIdentifier _ -> WS.sendTextData conn $ JSON.encode $ addVisited myself msg
             _ -> return ()
-        Studio -> WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+        Studio -> WS.sendTextData conn $ JSON.encode $ addVisited myself msg
         _ -> return ()
 
-routedMessage :: WS.Connection -> Client -> Message -> IO ()
+routedMessage :: WS.Connection -> Service -> Message -> IO ()
 routedMessage conn client msg = do
     -- route message from store to the correct service
     -- client is the currently connected ms
@@ -81,16 +83,16 @@ routedMessage conn client msg = do
                         InitiatedConnection _ -> return ()
                         -- send to ident :
                         AddedIdentifierType _ -> do
-                            WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                            WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                             putStrLn $ "Sent to " ++ show client ++ " through WS"
                         RemovedIdentifierType _ -> do
-                            WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                            WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                             putStrLn $ "Sent to " ++ show client ++ " through WS"
                         ChangedIdentifierType _ _ -> do
-                            WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                            WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                             putStrLn $ "Sent to " ++ show client ++ " through WS"
                         AddedIdentifier _ -> do
-                            WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                            WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                             putStrLn $ "Sent to " ++ show client ++ " through WS"
                         _ -> return ()
                 _ -> return ()
@@ -100,15 +102,15 @@ routedMessage conn client msg = do
                 Front -> case payload msg of
                     InitiatedConnection _ -> return ()
                     _ -> do
-                        WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                        WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                         putStrLn $ "Sent to " ++ show client ++ " through WS"
                 _ -> return ()
             Processed -> case creator msg of
                 Studio -> do
-                    WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                    WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                     putStrLn $ "Sent to " ++ show client ++ " through WS"
                 Ident -> do
-                    WS.sendTextData conn $ JSON.encode $ addVisited Store msg
+                    WS.sendTextData conn $ JSON.encode $ addVisited myself msg
                     putStrLn $ "Sent to " ++ show client ++ " through WS"
                 _ -> return ()
             _ -> return ()
@@ -163,7 +165,7 @@ serverApp msgPath chan stateMV pending_conn = do
                             -- send the InitiatedConnection terminaison to signal the sync is over
                             (WS.sendTextData conn . JSON.encode) $
                                 Message
-                                    (Metadata{uuid = uuid $ metadata msg, Metadata.when = when $ metadata msg, Metadata.from = [Store], Metadata.flow = Processed})
+                                    (Metadata{uuid = uuid $ metadata msg, Metadata.when = when $ metadata msg, Metadata.from = [myself], Metadata.flow = Processed})
                                     (InitiatedConnection (Connection{lastMessageTime = 0, Connection.uuids = Set.empty}))
                         _ -> Monad.when (messageId (metadata msg) `notElem` Main.uuids st') $ do
                             appendMessage msgPath msg
